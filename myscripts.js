e6e2c5282743e1aae93cadf4c1a8aaf236beab97
saul <saulc89@gmail.com>
2017-05-28 19:19:50 -0700

    autoupdate
    added onblur for auto update on form change, and on load for populated
    tables by default.
    
    calculate button is basically useless….

--- myscripts.js
+++ myscripts.js
@@ -7,11 +7,11 @@ function calc() {
     var text = s + "s" + p +"p " + (c*p) + "Ah";
     document.getElementById("batteryinfo").innerHTML = text;
     text = t*s;
-    document.getElementById("batteryvol").innerHTML = text;
+    document.getElementById("batteryvol").innerHTML = text.toFixed(2);
      text = p*c;
-    document.getElementById("batterycap").innerHTML = text;
+    document.getElementById("batterycap").innerHTML = text.toFixed(2);
      var be = t*s*p*c;
-    document.getElementById("batteryenergy").innerHTML = be;
+    document.getElementById("batteryenergy").innerHTML = be.toFixed(2);
 
     var kv =  document.getElementById("kv").value;
     var ef =  document.getElementById("eff").value;
@@ -23,16 +23,16 @@ function calc() {
     var wp =  document.getElementById("wp").value;
     var ratio = wp/mp;
     text = s*t*kv;
-    document.getElementById("motorrpm").innerHTML = text;
+    document.getElementById("motorrpm").innerHTML = text.toFixed(2);
     text = s*t*kv*ef;
-    document.getElementById("motorrpmload").innerHTML = text;
+    document.getElementById("motorrpmload").innerHTML = text.toFixed(2);
     text = s*t*kv*ef/ratio;
-    document.getElementById("wheelrpm").innerHTML = text;
+    document.getElementById("wheelrpm").innerHTML = text.toFixed(2);
     document.getElementById("ratio").innerHTML = ratio.toFixed(2);
     text = s*t*kv*poles;
-    document.getElementById("erpm").innerHTML = text;
+    document.getElementById("erpm").innerHTML = text.toFixed(2);
     text = s*t*kv*ef*poles;
-    document.getElementById("erpmload").innerHTML = text;
+    document.getElementById("erpmload").innerHTML = text.toFixed(2);
     var temp = wheel/1000000 *3.14159 *60
     text = temp * s*t*kv*ef/ratio ;
     document.getElementById("speedk").innerHTML = text.toFixed(2);
@@ -41,5 +41,7 @@ function calc() {
     var wpm = document.getElementById("wpm").value;
     var range = be/wpm;
      document.getElementById("range").innerHTML = range.toFixed(2);
+     range = range*1.609344;
+      document.getElementById("rangek").innerHTML = range.toFixed(2);
     
 }
